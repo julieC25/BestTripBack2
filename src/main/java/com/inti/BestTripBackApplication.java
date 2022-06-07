2455e2ab4ddea9043239168307860e95c4d56adc
root <root@localhost.localdomain>
2022-06-07 16:24:32 +0200

    first commit

---
+++ src/main/java/com/inti/BestTripBackApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTripBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTripBackApplication.class, args);
+	}
+
+}
